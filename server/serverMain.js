50ed123c806a7a7392da47c5d6c1daffbfebe8c7
Josh <josh5231@gmail.com>
2017-07-26 03:10:40 -0400

    Basic setup
    Added express, body-parser. Setup get request route for index.html.
    Place holder for slack api.

--- server/serverMain.js
+++ server/serverMain.js
@@ -1,1 +1,20 @@
 //Server's main code..
+"use strict";
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/api/*', (req,res) => {
+  res.status(200).send(req.body);
+});
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(__dirname+"/public/html/index.html");
+});
+
+module.exports = app;
+module.exports.port = port;
