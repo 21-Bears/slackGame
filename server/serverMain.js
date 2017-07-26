01dfba3b306669d547d141475e0455195c1c3249
Josh <josh5231@gmail.com>
2017-07-26 03:44:14 -0400

    test 4

--- server/serverMain.js
+++ server/serverMain.js
@@ -10,12 +10,15 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+//This is used for css and asset files
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.post('/api/*', (req,res) => {
   res.status(200).send(req.body);
 });
 
 app.get('/', (req, res) => {
-  res.status(200).sendFile( path.join( __dirname, '..', 'public', 'index.html' ) );
+  res.status(200).sendFile( path.join( __dirname, '..', 'public/html/', 'index.html' ) );
 });
 
 module.exports = app;
