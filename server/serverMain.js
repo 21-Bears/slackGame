8443fa45d3280e9d8afd55fc12c5991f8e5783f4
ReeceLangerock <ReeceLangerock@gmail.com>
2017-07-30 16:26:38 -0500

    first rough draft of landing page for slackbot

--- server/serverMain.js
+++ server/serverMain.js
@@ -1,1 +1,27 @@
 //Server's main code..
+const express = require('express');
+var path = require("path");
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+
+const app = express();
+app.port = 3000;
+app.use(bodyParser.json());
+
+
+app.post('/userInput',(req,res)=>{
+	let body = _.pick(req.body,'command');
+
+
+});
+app.use(express.static( __dirname + './../public/html'));
+app.use(express.static( __dirname + './../public/css'));
+app.use(express.static( __dirname + './../public/assets'));
+app.get('/',(req,res)=>{
+	res.sendFile(path.join(__dirname+'./../public/htmlindex.html'));
+
+
+});
+
+
+module.exports = {app};
