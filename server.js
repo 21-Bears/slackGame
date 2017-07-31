c6ac255810a3a32385a67f2fe31032d767765d24
nealthom <nealthom@gmail.com>
2017-07-31 03:51:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //The server entry point
-const app = require('./server/serverMain.js');
-const port = app.port;
+const {app} = require('./server/serverMain.js');
+const port = process.env.PORT || 3000;;
 
-app.listen(port, () => console.log(`listening on port: `+port) );
+app.listen(port, () => console.log(`listening on port: `+ port) );
