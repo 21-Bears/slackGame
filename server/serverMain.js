eabbacf87ad7a7534abaecfabe641259d1baa2e2
nealthom <nealthom@gmail.com>
2017-07-31 17:26:37 -0500

    Update serverMain.js

--- server/serverMain.js
+++ server/serverMain.js
@@ -1,65 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
+//const gameObject = require('./gameObj');
 const { createMessageAdapter } = require('@slack/interactive-messages');
 
 // Initialize using verification token from environment variables
-const slackMessages = createMessageAdapter(process.env.SLACK_VERIFICATION_TOKEN ||'AH4knYuu3IfcuMKzdrXQzbfE');
+const slackMessages = createMessageAdapter(process.env.SLACK_VERIFICATION_TOKEN ||'7AAmedaFMWoyhXzzhre0kUqM');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/slack/actions', slackMessages.expressMiddleware());
 
-let data = {  
-      "text": "presented by Bears21!",
-    	"attachments": [              
-        {            
-            "title": "Would you like to play slackGame?",
-            "callback_id": "startGame",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "yes",
-                    "text": "Yes",
-                    "type": "button",
-                    "value": "yes"
-                },
-                {
-                    "name": "no",
-                    "text": "No",
-                    "type": "button",
-                    "value": "no"
-                }
-            ]
-        }
-    ]
-};
-
 app.post('/',(req,res)=>{
-res.json(data);	
+let slackData = { user_name: req.body.user_name,
+              user_id: req.body.user_id,
+              ref_url: req.body.response_url};
+              
+res.status(200).send();	
 });
 
 slackMessages.action('startGame', (payload) => {
-  console.log(payload);
+  let slackData = { user_name: payload.user.name,
+              user_id: payload.user.id,
+              ref_url: payload.response_url,
+              action_name: payload.actions[0].name,
+              action_value: payload.actions[0].value
+            };
 
-  const action = payload.actions[0];
-  data.text = `Welcome ${payload.user.name}!`;
-  data.attachments[0].title = 'Would you like to start a new game or join one?';
-  data.callback_id = 'newOrJoin';
-  data.attachments[0].actions = [{
-                    "name": "newGame",
-                    "text": "New Game",
-                    "type": "button",
-                    "value": "yes"
-                },
-                {
-                    "name": "joinGame",
-                    "text": "Join Game",
-                    "type": "button",
-                    "value": "no"
-                }];
-return data;
+res.status(200).send();
   
 });
 
